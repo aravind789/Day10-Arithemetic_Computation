2f5ba8f4060d1956ba1e54c72e6d74b197ca0d8b
aravind789 <aravindkris89@gmail.com>
2020-07-02 22:59:42 +0530

    storing results in dictionary

--- arithmetic_computation_and_sorting.sh
+++ arithmetic_computation_and_sorting.sh
@@ -6,7 +6,15 @@ equation_1=$((($a+$b)*$c))
 equation_2=$((($a*$b)+$c))
 equation_3=$((($c+$a)/$b))
 equation_4=$((($a%$b)+$c))
+
 echo "Result of (a+b*c)=" $equation_1
 echo "Result of (a*b+c)=" $equation_2
 echo "Result of (c+a/b)=" $equation_3
 echo "Result of (a%b+c)=" $equation_4
+
+declare -i results
+results[1]=$equation_1
+results[2]=$equation_2
+results[3]=$equation_3
+results[4]=$equation_4
+echo ${results[@]}
