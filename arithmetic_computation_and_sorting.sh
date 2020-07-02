4fe58be9ec3f26bc6f77e046572fa0c483abecba
aravind789 <aravindkris89@gmail.com>
2020-07-02 22:43:19 +0530

    second equation

--- arithmetic_computation_and_sorting.sh
+++ arithmetic_computation_and_sorting.sh
@@ -3,4 +3,6 @@ read -p "Enter the value :" a
 read -p "Enter the value :" b
 read -p "Enter the value :" c
 equation_1=$((($a+$b)*$c))
+equation_2=$((($a*$b)+$c))
 echo "Result of (a+b*c)=" $equation_1
+echo "Result of (a*b+c)=" $equation_2
