7bc659dae91db2892e7c07b4962b3418fa9dc718
aravind789 <aravindkris89@gmail.com>
2020-07-02 22:51:58 +0530

    equation 4

--- arithmetic_computation_and_sorting.sh
+++ arithmetic_computation_and_sorting.sh
@@ -5,6 +5,8 @@ read -p "Enter the value :" c
 equation_1=$((($a+$b)*$c))
 equation_2=$((($a*$b)+$c))
 equation_3=$((($c+$a)/$b))
+equation_4=$((($a%$b)+$c))
 echo "Result of (a+b*c)=" $equation_1
 echo "Result of (a*b+c)=" $equation_2
 echo "Result of (c+a/b)=" $equation_3
+echo "Result of (a%b+c)=" $equation_4
